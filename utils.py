689f529ccf7bf500c01572d480215bbe61b5b17a
beckythebetts <rebecca@bettscrossland.me.uk>
2024-04-23 13:40:40 +0100

    ~

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import shutil
 def remake_dir(path):
     if path.is_dir():
         check = input(f'Delete directory {str(path)}? [y,n]')
-        if check.lower != 'y':
+        if check.lower() != 'y':
             print('Cancelled')
             return
         shutil.rmtree(path)
