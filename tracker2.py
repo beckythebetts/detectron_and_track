a5fcd87a860a936ec66708b7bf27dcb2c23df4e6
beckythebetts <rebecca@bettscrossland.me.uk>
2024-04-26 17:20:30 +0100

    ~

--- tracker2.py
+++ tracker2.py
@@ -41,27 +41,15 @@ class Tracker:
     def track(self):
 
         for i in range(len(self.mask_ims)):
-            try:
-                last_time = round(end_time - start_time, 1)
-                times = np.append(times, last_time)
-                average = round(np.average(times), 1)
-            except:
-                last_time = ' - '
-                average = ' - '
             sys.stdout.write(
                 f'\rAdding frame {i + 1} / {len(self.mask_ims)}\t|\tLast frame took {last_time} s\t|\tAverage time per frame is {average} s')
             sys.stdout.flush()
-            start_time = time.time()
-
 
             self.new_frame = torch.tensor(plt.imread(self.mask_ims[i+1]).astype(np.int16)).cuda()
             im = Image.fromarray(self.old_frame.cpu().numpy().astype(np.int16))
             im.save(SETTINGS.DIRECTORY / 'tracking' / self.name / ("{0:03}".format(i) + '.tif'))
             self.update_new_frame()
             self.old_frame = self.new_frame
-
-
-            end_time = time.time()
 
 def main():
     my_tracker = Tracker('Amoeba')
