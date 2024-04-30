edbb633660775943e274463564d395d724aa1afb
beckythebetts <rebecca@bettscrossland.me.uk>
2024-04-30 16:46:32 +0100

    ~

--- tracker2.py
+++ tracker2.py
@@ -90,6 +90,7 @@ class Tracker:
                     for c in range(3):
                         im_rgb[c] = torch.where(outline, colours[j, c], im_rgb[c])
                         print(np.shape(im_rgb[c]), np.shape(im_rgb))
+            im_rgb = im_rgb.permute(1, 2, 0)
             Image.fromarray((im_rgb*255).cpu().numpy().astype(np.uint8)).save(view_track_dir / (str(i)+'.jpg'))
 
 
