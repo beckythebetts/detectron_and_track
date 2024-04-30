f6d687372c66dcd010af6ac4b358ad50486b457d
beckythebetts <rebecca@bettscrossland.me.uk>
2024-04-30 16:40:19 +0100

    ~

--- tracker2.py
+++ tracker2.py
@@ -77,7 +77,7 @@ class Tracker:
             mask = torch.tensor(utils.read_tiff(self.tracked_masks[i]).astype(np.int16)).cuda()
             image = utils.torch_min_max_scale(torch.tensor(utils.read_tiff(self.images[i]).astype(np.int16)).cuda())
             im_rgb = torch.stack((image, image, image), axis=0)
-            print(mask.shape)
+            #print(mask.shape)
             #split_mask = [torch.where(mask == i + 1, 1, 0) for i in range(0, torch.max(mask)) if i + 1 in mask]
             for j in range(torch.max(mask)+1):
                 if j+1 in mask:
@@ -86,9 +86,9 @@ class Tracker:
                     expanded_mask = F.max_pool2d(single_mask.float().unsqueeze(1), kernel_size=3, stride=1, padding=1) > 0
                     #print(expanded_mask.shape)
                     outline = (expanded_mask.byte().squeeze() - single_mask).bool()
-                    print(outline.shape)
+                    #print(outline.shape)
                     for c in range(3):
-                        im_rgb[c] = torch.where(outline, torch.ones(size=outline.shape).cuda()*colours[j, c], im_rgb[c])
+                        im_rgb[c] = torch.where(outline, colours[j, c], im_rgb[c])
             Image.fromarray((im_rgb*255).cpu().numpy().astype(np.uint8)).save(view_track_dir / (str(i)+'.jpg'))
 
 
