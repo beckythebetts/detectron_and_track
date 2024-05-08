617e816d0aa541eab10875eae628e301e2b842e6
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-05-08 12:59:43 +0100

    ~

--- track.py
+++ track.py
@@ -118,7 +118,7 @@ class Tracker:
 
 
 def main():
-    trackers = [Tracker(cell_type) for cell_type in SETTINGS.CLASSES.values()]
+    #trackers = [Tracker(cell_type) for cell_type in SETTINGS.CLASSES.values()]
     trackers = [Tracker(name) for name in SETTINGS.CLASSES.keys()]
     if SETTINGS.TRACK:
         for tracker in trackers:
