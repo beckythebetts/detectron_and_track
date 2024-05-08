785551cc98fff133caf73360fcbc0e3dc47a455b
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-05-08 12:32:22 +0100

    ~

--- mask_funcs.py
+++ mask_funcs.py
@@ -33,8 +33,8 @@ class SplitMask:
         if self.i <= self.max:
             while self.i not in self.mask_full:
                 self.i += 1
-            return torch.where(self.mask_full == self.i, 1, 0), self.i
             self.i += 1
+            return torch.where(self.mask_full == self.i, 1, 0), self.i
         else:
             raise StopIteration
 
