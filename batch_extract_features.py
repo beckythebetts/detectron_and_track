2e7f8e13b125eb4fd349c288fd6f5a6af8bb36dc
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-05-15 14:56:24 +0100

    ~

--- batch_extract_features.py
+++ batch_extract_features.py
@@ -108,8 +108,8 @@ class CellBatch:
                     if dist_temp < dist or math.isnan(dist):
                         dist = dist_temp
                         index = self.epi_indices[i]
-            self.dists = self.dists.append(dist)
-            self.indices_of_nearest = self.indices_of_nearest.append(index)
+            self.dists.append(dist)
+            self.indices_of_nearest.append(index)
     def get_nearest(self):
         dists = torch.full((len(self.indices),), -1, dtype=torch.float64).cuda()
         indices_of_nearest = torch.full((len(self.indices),), -1, dtype=torch.float64).cuda()
