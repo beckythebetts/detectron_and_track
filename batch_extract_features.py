e4f1cf23c0c5f0b78ccecc0b111c32626ed46ae4
beckythebetts <rebecca@bettscrossland.me.uk>
2024-05-17 20:59:40 +0100

    ~

--- batch_extract_features.py
+++ batch_extract_features.py
@@ -133,9 +133,9 @@ class CellBatch:
         self.dists, i = torch.min(distances, dim=0)
 
     def get_eaten(self):
-        #intersection = torch.logical_and(self.masks, self.epi_masks.unsqueeze(-1))
-        print(self.masks.shape)
-        #unique = torch.unique(self.masks[intersection], dim=)
+        intersection = torch.logical_and(self.masks, self.epi_masks.unsqueeze(0))
+        unique = torch.unique(self.masks[intersection], dim=(1, 2))
+        print(unique)
 
 
 def plot_features():
