e1491bc173289a4e4548b09ff19b267987ef5bc2
beckythebetts <rebecca@bettscrossland.me.uk>
2024-05-22 11:50:56 +0100

    ~

--- detectron_train.py
+++ detectron_train.py
@@ -7,7 +7,7 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultTrainer, launch, HookBase
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset, infernece_context
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset, inference_context
 from detectron2.data import build_detection_test_loader
 from detectron2.utils.visualizer import ColorMode
 from detectron2.structures import Instances
