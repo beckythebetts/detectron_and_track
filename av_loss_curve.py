6e6d8e60ea23d46a35ff5a230bfbf28567018a74
beckythebetts <rebecca@bettscrossland.me.uk>
2024-05-30 12:48:33 +0100

    ~

--- av_loss_curve.py
+++ av_loss_curve.py
@@ -6,6 +6,8 @@ from pathlib import Path
 def plot_average_loss_curves(directory):
 
     for folder in directory.iterdir():
+        all_train_losses = []
+        all_val_lossees = []
         with open(folder / 'model' / 'metrics.json', 'r') as f:
             lines = []
             for line in f:
@@ -15,7 +17,10 @@ def plot_average_loss_curves(directory):
 
             val_losses = [x['validation_loss'] for x in lines if 'validation_loss' in x]
             val_iters = [x['iteration'] for x in lines if 'validation_loss' in x]
-            print(train_iters, val_iters)
+
+            all_train_losses = np.stack(all_train_losses, train_losses)
+            all_val_losses = np.stack(all_val_losses, val_losses)
+            print(train_losses.shape)
 
 def main():
     plot_average_loss_curves(Path('kfold_quarter_old') / 'train6')
