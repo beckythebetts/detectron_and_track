52cb9e160b4775ce27a01c4b5f7822b1d09229b0
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-04 11:45:59 +0100

    ~

--- plot_tracks.py
+++ plot_tracks.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import SETTINGS
 import utils
+import mask_funcs
 
 def plot_tracks(dir):
     track_plot = torch.zeros(*SETTINGS.IMAGE_SIZE, 3)
@@ -12,7 +13,7 @@ def plot_tracks(dir):
     for file in sorted([i for i in dir.iterdir()]):
         frame = torch.tensor(utils.read_tiff(file).astype(np.int32)).cuda()
         for i in torch.unique(frame):
-            centre = utils.find_centre(torch.where(frame==i))
+            centre = mask_funcs.find_centre(torch.where(frame==i))
             if i not in colours_dict:
                 colours_dict[i] = torch.tensor(np.random.uniform(0, 2**(8)-1, size=3)).cuda()
             track_plot[centre, :] = colours_dict[i]
