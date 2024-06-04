b500b670a59c4f5bec6e96597dfe5c91dcb53151
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-04 17:17:33 +0100

    ~

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ def draw_line(array, x0, x1, y0, y1, colour):
         x0, y0, x1, y1 = y0, x0, y1, x1
     x = torch.arange(x0, x1 + 1).cuda()
     y = ((y1-y0)/(x1-x0))*(x-x0) + y0
-    array[x.round, y.round, :] = colour
+    array[x.round().to(torch.int16), y.round().to(torch.int16), :] = colour
     return array if not transpose else array.T
 
 if __name__ == '__main__':
