e140d4f08423e149a930a517be9a8ffd7bc4722f
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-04 11:45:10 +0100

    ~

--- plot_tracks.py
+++ plot_tracks.py
@@ -10,13 +10,13 @@ def plot_tracks(dir):
     track_plot = torch.zeros(*SETTINGS.IMAGE_SIZE, 3)
     colours_dict = {}
     for file in sorted([i for i in dir.iterdir()]):
-        frame = torch.tensor(utils.read_tiff(file)).cuda()
+        frame = torch.tensor(utils.read_tiff(file).astype(np.int32)).cuda()
         for i in torch.unique(frame):
             centre = utils.find_centre(torch.where(frame==i))
             if i not in colours_dict:
                 colours_dict[i] = torch.tensor(np.random.uniform(0, 2**(8)-1, size=3)).cuda()
             track_plot[centre, :] = colours_dict[i]
-    utils.save_tiff(tracK_plot, dir.parents[2] / 'track_plot.png')
+    utils.save_tiff(track_plot, dir.parents[2] / 'track_plot.png')
 
 
 def main():
