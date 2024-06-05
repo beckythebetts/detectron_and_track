b41aba338e195f8f0db1038bbec0f6ab295a7ae6
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-05 10:27:16 +0100

    ~

--- batch_extract_features.py
+++ batch_extract_features.py
@@ -142,14 +142,14 @@ def plot_tracks():
     print('\n---------------\nPlotting Tracks\n---------------\n')
     for features in (SETTINGS.DIRECTORY / 'features').iterdir():
         data = pd.read_csv(features, sep='\t')
-        colour = torch.tensor(np.random.uniform(0, 1, size=3)).cuda()
+        colour = torch.tensor(np.random.uniform(0, 2**(8)-1, size=3)).cuda()
         centres = torch.tensor(data.loc[:, 'xcentre':'ycentre'].values).cuda()
         #print(data.loc[:, 'xcentre':''])
         for i in range(len(centres) - 1)[:10]:
             if not torch.any(centres[i:i+2].isnan()):
                 tracks_plot = utils.draw_line(tracks_plot, centres[i, 0], centres[i+1, 0], centres[i, 1], centres[i+1, 1], colour)
                 print(tracks_plot.shape)
-    utils.save_tiff(tracks_plot.cpu().numpy(), SETTINGS.DIRECTORY / 'tracks_plot.png')
+    utils.save_tiff(tracks_plot.cpu().numpy().astype(np.uint8), SETTINGS.DIRECTORY / 'tracks_plot.png')
 
 def plot_features():
     print('\n---------------\nPlotting Features\n---------------\n')
