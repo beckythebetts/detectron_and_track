1d6d3cdabaaf142467e1b0d44e999da8159d46b7
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-15 16:54:37 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -14,7 +14,7 @@ OVERLAP_THRESHOLD = 0.2
 FRAME_MEMORY = 3
 TRACK = True
 VIEW_TRACKS = True # Save labelled tracked images
-NUM_FRAMES_TO_VIEW = None # Set as None to view all (slow)
+NUM_FRAMES_TO_VIEW = 50 # Set as None to view all (slow)
 
 
 # ******* FEATURE EXTRACTION *******
