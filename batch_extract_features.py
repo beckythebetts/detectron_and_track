592cd666ac2f94a6a078c9c85b662528b65d025e
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-17 12:38:50 +0100

    ~

--- batch_extract_features.py
+++ batch_extract_features.py
@@ -152,13 +152,15 @@ def plot_tracks():
     utils.save_tiff(tracks_plot.cpu().numpy().astype(np.uint8), SETTINGS.DIRECTORY / 'tracks_plot.png')
 
 def plot_features():
+    plt.rcParams["font.family"] = 'serif'
     print('\n---------------\nPlotting Features\n---------------\n')
     utils.remake_dir(SETTINGS.DIRECTORY / 'features_plots')
     for features_path in (SETTINGS.DIRECTORY / 'features').iterdir():
         data = pd.read_csv(features_path, sep='\t')
         fig, axs = plt.subplots(5, sharex=True, figsize=(10, 10))
+        colours=['firebrick', 'darkorange', 'yellowgreen', 'lightseagreen', 'royalblue']
         for i in range(5):
-            axs[i].plot(data.iloc[:, i])
+            axs[i].plot(data.iloc[:, i], color=colours[i])
             axs[i].set(ylabel=data.columns.values.tolist()[i])
             axs[i].grid()
 
@@ -198,14 +200,20 @@ def show_eating():
 def main():
     torch.cuda.empty_cache()
     gc.enable()
-    # with torch.no_grad():
-    #     utils.remake_dir(SETTINGS.DIRECTORY / 'features')
-    #     cell_batch = CellBatch(torch.tensor(np.arange(1, 101)).cuda())
-    #     cell_batch.run_feature_extraction()
-    # if SETTINGS.PLOT_FEATURES:
-    #     plot_features()
-    # if SETTINGS.TRACKS_PLOT:
-    #     plot_tracks()
+    with torch.no_grad():
+        utils.remake_dir(SETTINGS.DIRECTORY / 'features')
+        cell_batch = CellBatch(torch.tensor(np.arange(1, 101)).cuda())
+        cell_batch.run_feature_extraction()
+        cell_batch = CellBatch(torch.tensor(np.arange(101, 201)).cuda())
+        cell_batch.run_feature_extraction()
+        cell_batch = CellBatch(torch.tensor(np.arange(201, 301)).cuda())
+        cell_batch.run_feature_extraction()
+        cell_batch = CellBatch(torch.tensor(np.arange(301, 401)).cuda())
+        cell_batch.run_feature_extraction()
+    if SETTINGS.PLOT_FEATURES:
+        plot_features()
+    if SETTINGS.TRACKS_PLOT:
+        plot_tracks()
     if SETTINGS.SHOW_EATING:
         show_eating()
 if __name__ == '__main__':
