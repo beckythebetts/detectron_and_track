c9edfe7aeae6f15e952a753045816badb0f836ed
beckythebetts <rebecca@bettscrossland.me.uk>
2024-06-26 15:54:24 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -12,11 +12,10 @@ THRESHOLD = 17469
 # ******* TRACKING *******
 OVERLAP_THRESHOLD = 0.2
 FRAME_MEMORY = 3
-TRACK = True
-CLEAN_TRACKS = False
-VIEW_TRACKS = False # Save labelled tracked images
+TRACK = False
+CLEAN_TRACKS = True
+VIEW_TRACKS = True # Save labelled tracked images
 NUM_FRAMES_TO_VIEW = 50 # Set as None to view all (slow)
-
 
 # ******* FEATURE EXTRACTION *******
 BATCH_SIZE = 100
