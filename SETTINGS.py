e40273092ab5f5eff4713627e6edabe83b6a940b
beckythebetts <rebecca@bettscrossland.me.uk>
2024-08-20 13:44:59 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -21,7 +21,7 @@ NUM_FRAMES_TO_VIEW = 50 # Set as None to view all (slow)
 
 # ******* FEATURE EXTRACTION *******
 BATCH_SIZE = 100
-PLOT_FEATURES = False
+PLOT_FEATURES = True
 TRACKS_PLOT = True
 SHOW_EATING = True
 
