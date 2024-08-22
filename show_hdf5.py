c0618996b9be965e7326f974d902fa241da658b5
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-08-22 15:07:01 +0100

    ~

--- show_hdf5.py
+++ show_hdf5.py
@@ -2,10 +2,10 @@ import h5py
 import imagej
 import numpy as np
 
-
 #from jnius import autoclass
 
-hdf5_file = 'Datasets/04_short.h5'
+#hdf5_file = 'Datasets/04_short.h5'
+hdf5_file = 'Datasets/danhighres/dan10.h5'
 #ij = imagej.init('sc.fiji:fiji:2.1.0', mode='gui')
 ij = imagej.init('2.1.0', mode='interactive')
 
@@ -30,7 +30,7 @@ def show_separate_channels():
         ij.ui().show('images', ij_image)
     input("Close ImageJ [ENTER]")
 
-def show_merged_channels_2():
+def show_merged_channels():
     with h5py.File(hdf5_file, 'r') as f:
         phase_data = np.array([f['Images']['Phase'][frame][:]
                                for frame in f['Images']['Phase'].keys()], dtype='uint8')
@@ -43,34 +43,16 @@ def show_merged_channels_2():
         print(merged_data.shape)
         merged_image = ij.py.to_dataset(merged_data, dim_order=['time', 'z', 'ch', 'row', 'col'])
         ij.ui().show(merged_image)
+        macro = """
+        run("Make Composite")
+        """
+        ij.py.run_macro(macro)
 
     input("Close ImageJ [ENTER]")
-    #return merged_data
 
-def show_merged_channels():
-    with h5py.File(hdf5_file, 'r') as f:
-        frames = list(f['Images']['Phase'].keys())
-        merged_images = []
-        for t in frames:
-            phase_image = f['Images']['Phase'][t][:][np.newaxis,:]
-            epi_image = f['Images']['Epi'][t][:][np.newaxis,:]
-
-            Phase = ij.py.to_java(phase_image)
-            Epi = ij.py.to_java(epi_image)
-
-            ij.ui().show('Phase', Phase)
-            ij.ui().show('Epi', Epi)
-
-            merged = ij.IJ.run("Merge Channels...", "c1=Phase c2=Epi create")
-
-            merged_images.append(merged)
-
-    hyperstack = np.stack(merged_images, axis=0)
-    ij.ui().show("Merged Hyperstack", ij.py.to_java(hyperstack))
-    input('Close ImageJ [ENTER]')
 
 def main():
-    show_separate_channels()
-    #show_merged_channels_2()
+    #show_separate_channels()
+    show_merged_channels()
 if __name__ == '__main__':
     main()
