18e08ff10772bdd07f7bc1365486ad2316da96ed
beckythebetts <rebecca@bettscrossland.me.uk>
2024-08-28 16:28:32 +0100

    ~

--- track_phagocytosis_events.py
+++ track_phagocytosis_events.py
@@ -5,28 +5,15 @@ import h5py
 import SETTINGS
 import utils
 
-# class PhagocyticEvent:
-#     def __init__(self, frames):
-#         self.frames = start_frame
-#         self.pathogen_indices = self.get_pathogen_indices()
-#
-#     def get_pathogen_indices(self):
-#
-#         self.pathogen_indices = [torch.unique(array) for array in
-#                                  torch.where(intersection, self.epi_mask.unsqueeze(0), 0)]
-#         self.pathogen_indices = [a[a.nonzero()] for a in self.pathogen_indices]
-#
-#
-# class PhagocyticCell:
-#     def __init__(self, cell_name, consecutive_eaten_frames):
-#         self.cell_name = cell_name
-#         self.cell_index = int(cell_name[-4:])
-#         self.consecutive_eaten_frames = consecutive_eaten_frames
-#         for sequence in consecutive_eaten_frames:
-#             if len(sequence) >= SETTINGS.NUM_FRAMES_EATEN_THRESHOLD:
-#                 phagocytic_event = PhagocyticEvent()
-#
-#     def track_internal_pathogens(self):
+class PhagocyticEvent:
+
+    def __init__(self, frames, pathogen_indices):
+        self.frames = frames
+        self.pathogen_indices = pathogen_indices
+
+    def add_frame(self, frame, index):
+        self.frames = np.append(self.frames, frame)
+        self.pathogen_indices = np.append(self.pathogen_indices, index)
 
 
 
@@ -34,9 +21,24 @@ def track_phagocytic_events(hdf5file):
     with h5py.File(hdf5file, 'r') as f:
         for cell in f['Features']:
             phago_events = f['Features'][cell]['PhagocyticFrames'][:]
-            phago_events = [event for event in phago_events if event[2]>10]
-            for event in phago_events:
-                print(phago_events)
+            # for event in phago_events:
+            #     print(phago_events)
+            #
+            frames = f['Features'][cell]['PhagocyticFrames']['frame'][:]
+            sequences = split_list_into_sequences(frames)
+            for sequence in sequences:
+                # if sequence of frames contains duplicate values => multiple pathogens are observed simultaneously => need tracking
+                if len(sequences) == len(set(sequences)):
+                    # if only one pathogen observed, no need to track
+                    indices = [phago_events[frame.index()][1] for frame in sequence]
+                    phago_event = PhagocyticEvent(sequence, indices)
+                # else:
+                #     # this phagocytic event involves multiple pathogens, so each must be tracked individually
+                #     for frame in np.unique(sequence):
+
+
+
+
 
 
 
