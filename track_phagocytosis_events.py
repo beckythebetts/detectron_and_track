dafbed572ac7350c0a265b8d2f5ad424711b133d
beckythebetts <rebecca@bettscrossland.me.uk>
2024-08-28 14:15:52 +0100

    ~

--- track_phagocytosis_events.py
+++ track_phagocytosis_events.py
@@ -5,38 +5,36 @@ import h5py
 import SETTINGS
 import utils
 
-class PhagocyticEvent:
-    def __init__(self, frames):
-        self.frames = start_frame
-        self.pathogen_indices = self.get_pathogen_indices()
-
-    def get_pathogen_indices(self):
-
-        self.pathogen_indices = [torch.unique(array) for array in
-                                 torch.where(intersection, self.epi_mask.unsqueeze(0), 0)]
-        self.pathogen_indices = [a[a.nonzero()] for a in self.pathogen_indices]
-
-
-class PhagocyticCell:
-    def __init__(self, cell_name, consecutive_eaten_frames):
-        self.cell_name = cell_name
-        self.cell_index = int(cell_name[-4:])
-        self.consecutive_eaten_frames = consecutive_eaten_frames
-        for sequence in consecutive_eaten_frames:
-            if len(sequence) >= SETTINGS.NUM_FRAMES_EATEN_THRESHOLD:
-                phagocytic_event = PhagocyticEvent()
-
-    def track_internal_pathogens(self):
+# class PhagocyticEvent:
+#     def __init__(self, frames):
+#         self.frames = start_frame
+#         self.pathogen_indices = self.get_pathogen_indices()
+#
+#     def get_pathogen_indices(self):
+#
+#         self.pathogen_indices = [torch.unique(array) for array in
+#                                  torch.where(intersection, self.epi_mask.unsqueeze(0), 0)]
+#         self.pathogen_indices = [a[a.nonzero()] for a in self.pathogen_indices]
+#
+#
+# class PhagocyticCell:
+#     def __init__(self, cell_name, consecutive_eaten_frames):
+#         self.cell_name = cell_name
+#         self.cell_index = int(cell_name[-4:])
+#         self.consecutive_eaten_frames = consecutive_eaten_frames
+#         for sequence in consecutive_eaten_frames:
+#             if len(sequence) >= SETTINGS.NUM_FRAMES_EATEN_THRESHOLD:
+#                 phagocytic_event = PhagocyticEvent()
+#
+#     def track_internal_pathogens(self):
 
 
 
 def track_phagocytic_events(hdf5file):
     with h5py.File(hdf5file, 'r') as f:
         for cell in f['Features']:
-            data = pd.DataFrame(f['Features'][cell][:])
-            eaten_frames = data.index[data['eaten'] >= 10].tolist()
-            if len(eaten_frames) > 0:
-                phagocytic_cell = PhagocyticCell(cell, consecutive_eaten_frames = utils.split_list_into_sequences(eaten_frames))
+            frames = f['Features'][cell]['PhagocyticFrames']['frame'][:]
+            print(frames)
 
 
 
