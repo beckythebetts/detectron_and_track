4304a16a55d6c8da23236994b2418238d7f4ee34
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-05 13:01:17 +0100

    ~

--- track_phagocytosis_events.py
+++ track_phagocytosis_events.py
@@ -32,21 +32,24 @@ def track_phagocytic_events(hdf5file):
         for cell in f['Features']:
             # sys.stdout.write(f'\r{cell}')
             # sys.stdout.flush()
-            phago_events = f['Features'][cell]['PhagocyticFrames'][:]
+            phago_events = f['Features'][cell]['PhagocyticFrames']
             frames = f['Features'][cell]['PhagocyticFrames']['frame'][:]
+            #print(phago_events['pathogen_index'])
             if len(frames) > SETTINGS.NUM_FRAMES_EATEN_THRESHOLD:
                 sequences = utils.split_list_into_sequences(frames)
                 for sequence in sequences:
                     # if sequence of frames contains duplicate values => multiple pathogens are observed simultaneously => need tracking
                     if len(sequence) == len(set(sequence)):
                         # if only one pathogen observed, no need to track
-                        indices = [event[1] for event in phago_events if event[0] in sequence]
+                        indices = [event[1] for event in phago_events[:] if event[0] in sequence]
+                        #print(indices)
                         # phago_event = PhagocyticEvent(sequence, indices)
                         # phago_event.save_event(cell)
                     else:
                         # this phagocytic event involves multiple pathogens, so each must be tracked individually
-                        print('FOUND ', cell, sequence[0])
-                        #for frame in np.unique(sequence):
+                        print('FOUND ', cell, sequence[0] )
+                        for i, frame in enumerate(np.unique(sequence)):
+                            print(cell, frame, phago_events['pathogen_index'][np.argwhere(frames==frame)])
 
 
 def show_phagocytic_events(dataset, save_directory):
