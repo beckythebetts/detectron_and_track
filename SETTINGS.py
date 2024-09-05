a775e2a78e02ee51d7d80c7ac7b14be442b0dfbc
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-05 13:02:37 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import h5py
 
 # ******* GENERAL *******
-#DATASET = Path("Datasets") / 'filter_test' / 'no_filter00.h5'
+DATASET = Path("Datasets") / 'filter_test' / 'no_filter00.h5'
 #DATASET = Path("Datasets") / 'interval_test' / '3sec.hdf5'
-DATASET = Path("Datasets") / '04_short_testing.h5'
+#DATASET = Path("Datasets") / '04_short_testing.h5'
 MASK_RCNN_MODEL = Path("Models") / 'filter_test'
 CLASSES = {'phase': 'Amoeba', 'epi': 'Yeast'}
 IMAGE_SIZE = (2048, 2048)
