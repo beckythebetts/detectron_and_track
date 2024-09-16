26636994c01312b668e64ba438a89d3e3009709f
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-09-16 11:05:48 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -2,10 +2,10 @@ from pathlib import Path
 import h5py
 
 # ******* GENERAL *******
-# DATASET = Path("Datasets") / 'filter_test' / 'no_filter00.h5'
+DATASET = Path("Datasets") / 'filter_test' / 'no_filter00.h5'
 #DATASET = Path("Datasets") / 'interval_test' / '3sec.hdf5'
 # DATASET = Path("Datasets") / '04_short_testing.h5'
-DATASET = Path('Datasets') / 'danhighres' / 'dan10.h5'
+#DATASET = Path('Datasets') / 'danhighres' / 'dan10.h5'
 MASK_RCNN_MODEL = Path("Models") / 'filter_test'
 CLASSES = {'phase': 'Amoeba', 'epi': 'Yeast'}
 IMAGE_SIZE = (2048, 2048)
--- threshold_epi.py
+++ threshold_epi.py
@@ -8,21 +8,33 @@ import sys
 import numpy as np
 import h5py
 
+def test_threshold(test_threshold_value):
+    with h5py.File(SETTINGS.DATASET, 'r') as f:
+        test_image = f['Images']['Epi'][list(f['Images']['Epi'].keys())[0]][...]
+        mask = np.where(test_image > test_threshold_value, 1, 0)
+        test_image = np.stack((test_image, test_image, test_image), axis=-1)
+        print(test_image.shape)
+        test_image[:,:,1] = np.where(mask, 1, test_image[:,:,1])
+        plt.imshow(test_image)
+        plt.show()
 
-def main():
+def apply_threshold(threshold=SETTINGS.THRESHOLD):
     print('\n--------------------\nTHRESHOLDING - ', SETTINGS.CLASSES['epi'], '\n--------------------')
     with h5py.File(SETTINGS.DATASET, 'r+') as f:
         if 'Epi' in f['Segmentations']:
-            del(f['Segmentations']['Epi'])
+            del (f['Segmentations']['Epi'])
         num_frames = f['Images'].attrs['Number of frames']
         for i, frame in enumerate(f['Images']['Epi']):
             sys.stdout.write(
                 f'\rAdding frame {i + 1} / {num_frames}')
             sys.stdout.flush()
             image = f['Images']['Epi'][frame][...]
-            image = np.where(image > SETTINGS.THRESHOLD, 1, 0)
+            image = np.where(image > threshold, 1, 0)
             image, num_objects = label(image)
             f.create_dataset(f'Segmentations/Epi/{frame}', dtype='i2', data=image)
+
+def main():
+    test_threshold(200)
 
 
 if __name__ == '__main__':
