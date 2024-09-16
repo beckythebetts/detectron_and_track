62184db368e4cdc05efafd12f9af2736a8eaba9a
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-09-16 11:30:09 +0100

    ~

--- threshold_epi.py
+++ threshold_epi.py
@@ -34,7 +34,7 @@ def apply_threshold(threshold=SETTINGS.THRESHOLD):
             f.create_dataset(f'Segmentations/Epi/{frame}', dtype='i2', data=image)
 
 def main():
-    test_threshold(200)
+    test_threshold(100)
 
 
 if __name__ == '__main__':
