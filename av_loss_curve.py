566a2fdd4a517aa98234d1ddae0f10fd17e02cf0
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-09-19 12:23:38 +0100

    ~

--- av_loss_curve.py
+++ av_loss_curve.py
@@ -44,7 +44,7 @@ def plot_average_loss_curves(directory):
     plt.savefig(directory / 'av_loss_plot.png')
 
 def main():
-    plot_average_loss_curves(Path('Models') / '04_orig')
+    plot_average_loss_curves(Path('Models') / '04_50_5_5')
 
 if __name__ == '__main__':
     main()
