b334861ba94f90f33b8ff050ba04607cf0dbc7a7
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-24 12:49:42 +0100

    ~

--- cellpose_train.py
+++ cellpose_train.py
@@ -1,11 +1,12 @@
-from cellpose import io, models, train, core
+from cellpose import io, models, core
+
 
 
 def train(train_directory, test_directory):
     use_GPU = core.use_gpu()
     io.logger_setup()
     images, labels, image_names, test_images, test_labels, image_names_test = io.load_train_test_data(train_directory, test_directory, image_filter='im', mask_filter='mask')
-    model = models.CellposeModel(model_type='cyto3')
+    model = models.CellposeModel(gpu=use_GPU, model_type='cyto3')
     model_path, train_losses, test_losses = train.train_seg(model.net,
                                                             train_data=images, train_labels=labels,
                                                             channels=[0, 0], normalize=True,
