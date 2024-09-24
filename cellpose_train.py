3e79f392f6be7b0d9be2e198329bb708c9ff8c3e
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-24 16:09:44 +0100

    ~

--- cellpose_train.py
+++ cellpose_train.py
@@ -6,16 +6,16 @@ import json
 def cellpose_train(directory):
     use_GPU = core.use_gpu()
     io.logger_setup()
-    images, labels, image_names, test_images, test_labels, image_names_test = io.load_train_test_data(directory / 'train', directory / 'validate', image_filter='im', mask_filter='mask')
+    images, labels, image_names, test_images, test_labels, image_names_test = io.load_train_test_data(str(directory / 'train'), str(directory / 'validate'), image_filter='im', mask_filter='mask')
     model = models.CellposeModel(gpu=use_GPU, model_type='cyto3')
     model_path, train_losses, test_losses = train.train_seg(model.net,
                                                             train_data=images, train_labels=labels,
                                                             channels=[0, 0], normalize=True,
                                                             test_data=test_images, test_labels=test_labels,
                                                             weight_decay=1e-4, SGD=True, learning_rate=0.1,
-                                                            n_epochs=100, save_path=directory, model_name='model')
+                                                            n_epochs=100, save_path=str(directory), model_name='model')
     losses_dict = {'Train Losses': train_losses, 'Validation Losses': test_losses}
-    with open(directory / 'losses.txt') as f:
+    with open(str(directory / 'losses.txt')) as f:
         json.dump(losses_dict, f)
 
 
