db022f8dd85bffc846708d728a98c71b7038466a
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-24 17:54:58 +0100

    ~

--- cellpose_eval.py
+++ cellpose_eval.py
@@ -7,13 +7,13 @@ import SETTINGS
 def cellpose_eval(directory):
     use_GPU = core.use_gpu()
     io.logger_setup()
-    model = models.CellposeModel(gpu=use_GPU, pretrained_model=SETTINGS.CELLPOSE_MODEL / 'models' / 'model')
+    model = models.CellposeModel(gpu=use_GPU, pretrained_model=str(SETTINGS.CELLPOSE_MODEL / 'models' / 'model'))
     im_names = [im.stem[:2] for im in directory.iterdir() if 'im' in im.stem]
     validation_ims = [io.imread(im) for im in directory.iterdir() if 'im' in im.name]
     channels = [0, 0]
     masks, flow, styles = model.eval(validation_ims)
     for mask, im_name in zip(masks, im_names):
-        plt.imave(directory/f'{im_name}pred.png' / masks)
+        plt.imave(str(directory/f'{im_name}pred.png'), mask)
 
 
 def main():
