fffd874be99af8099fb6e5bef5c47ffb514ae425
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-24 15:57:41 +0100

    ~

--- cellpose_train.py
+++ cellpose_train.py
@@ -7,12 +7,12 @@ def cellpose_train(train_directory, test_directory):
     io.logger_setup()
     images, labels, image_names, test_images, test_labels, image_names_test = io.load_train_test_data(train_directory, test_directory, image_filter='im', mask_filter='mask')
     model = models.CellposeModel(gpu=use_GPU, model_type='cyto3')
-    model_path = train.train_seg(model.net,
+    model_path, train_losses, test_losses = train.train_seg(model.net,
                                                             train_data=images, train_labels=labels,
                                                             channels=[0, 0], normalize=True,
                                                             test_data=test_images, test_labels=test_labels,
                                                             weight_decay=1e-4, SGD=True, learning_rate=0.1,
-                                                            n_epochs=5, save_path='cellpose_Models', model_name='01')
+                                                            n_epochs=100, save_path='cellpose_Models', model_name='filter01')
 
 def main():
     cellpose_train(r'/home/ubuntu/Documents/detectron_and_track/cellpose_Models/filters/train', r'/home/ubuntu/Documents/detectron_and_track/cellpose_Models/filters/validate')
