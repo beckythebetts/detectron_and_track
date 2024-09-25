5baf1666c330df97c4dbe5382e93a3d0a731cf8f
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-25 11:15:41 +0100

    ~

--- cellpose_eval.py
+++ cellpose_eval.py
@@ -11,10 +11,12 @@ def cellpose_eval(directory):
     im_names = [im.stem[:2] for im in directory.iterdir() if 'im' in im.stem]
     validation_ims = [io.imread(im) for im in directory.iterdir() if 'im' in im.name]
     channels = [0, 0]
-    masks, flow, styles = model.eval(validation_ims)
-    for mask, im_name in zip(masks, im_names):
-        plt.imsave(str(directory/f'{im_name}pred.png'), mask, cmap='gray')
-
+    preds, flow, styles = model.eval(validation_ims)
+    for pred, im_name in zip(preds, im_names):
+        plt.imsave(str(directory/f'{im_name}pred.png'), pred, cmap='gray')
+    true_masks = [io.imread(im) for im in directory.iterdir() if 'mask' in im.name]
+    APs, TPs, FPs, FNs = cellpose.metrics.average_precision(true_masks, preds)
+    print(APs, TPs, FPs, FNs)
 
 def main():
     cellpose_eval(SETTINGS.CELLPOSE_MODEL / 'validate')
