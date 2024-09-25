9a580ae2421c8f81d5df1b0cd063b746585753e3
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-25 17:07:33 +0100

    ~

--- detectron_eval.py
+++ detectron_eval.py
@@ -67,8 +67,10 @@ def eval_with_cellpose(directory):
     validation_ims = [plt.imread(str(im)) for im in (directory/'Training_Data'/'validate'/'Images').iterdir()]
     predicted_masks = np.array([])
     for im in validation_ims:
-        detectron_outputs = predictor(np.stack([np.array(im)] * 3, axis=-1))
-        print(detectron_outputs)
+        im_RGB = np.stack([np.array(im)] * 3, axis=-1)
+        print('CHECK IMAGE', np.min(im_RGB), np.max(im_RGB))
+        detectron_outputs = predictor(im_RGB)
+        #print(detectron_outputs)
         class_masks = {class_name: torch.zeros_like(detectron_outputs["instances"].pred_masks[0], dtype=torch.int16,
                                                     device=device)
                        for class_name in train_metadata['thing_classes']}
