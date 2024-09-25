0adfeb2a80f1725ef5f250f6ed22ccc13c49cced
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-25 17:33:43 +0100

    ~

--- cellpose_eval.py
+++ cellpose_eval.py
@@ -34,6 +34,25 @@ def cellpose_eval(directory):
         #view_frame.show_frame(str(directory / f'{im_name}im.png'), str(directory /f'{im_name}pred.png'), str(directory /f'{im_name}_view.png'))
         plt.imsave(str(directory /f'{im_name}_view.png'), utils.show_segmentation(np.array(validation_ims[i]), np.array(preds[i]).astype(np.int16), np.array(true_masks[i]).astype(np.int16)))
 
+def cellpose_eval_from_ims(directory):
+    image = [io.imread(str(directory/'Images'/'snap01.png'))]
+    mask = [io.imread(str(directory/'Masks'/'01mask.png'))]
+    pred = [io.imread(str(directory/'Masks'/'01pred.png'))]
+    thresholds = [0.5, 0.75, 0.9]
+    APs, TPs, FPs, FNs = metrics.average_precision(mask, pred, threshold=thresholds)
+    precisions = TPs / (TPs + FPs)
+    recalls = TPs / (TPs + FNs)
+    F1s = TPs / (TPs + 0.5 * (FPs + FNs))
+
+    df = pd.DataFrame({'Precision': precisions,
+                       'Recall': recalls,
+                       'F1': F1s},
+                      index=thresholds)
+    df.to_csv(str(directory / f'{im_name}_results.txt'), sep='\t')
+    # view_frame.show_frame(str(directory / f'{im_name}im.png'), str(directory /f'{im_name}pred.png'), str(directory /f'{im_name}_view.png'))
+    plt.imsave(str(directory / f'{im_name}_view.png'),
+               utils.show_segmentation(np.array(image), np.array(pred).astype(np.int16),
+                                       np.array(mask).astype(np.int16)))
 # class Results:
 #     def __init__(self, model, threshold, files):
 #         self.model = models
@@ -45,7 +64,7 @@ def cellpose_eval(directory):
 # def plot_results(cellpose_results, rcnn_results):
 #     cell
 def main():
-    cellpose_eval(SETTINGS.CELLPOSE_MODEL / 'validate')
-
+    #cellpose_eval(SETTINGS.CELLPOSE_MODEL / 'validate')
+    cellpose_eval_from_ims(SETTINGS.MASK_RCNN_MODEL / 'Training_Data' / 'validate')
 if __name__ == '__main__':
     main()
