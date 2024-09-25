8b82646d5e8c9923047419fe5ae354e4b6838fa1
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-25 15:49:35 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -12,7 +12,7 @@ CLASSES = {'phase': 'Amoeba', 'epi': 'Yeast'}
 REMOVE_EDGE_CELLS = True
 with h5py.File(DATASET, 'r') as f:
     NUM_FRAMES = f['Images'].attrs['Number of frames']
-    IMAGE_SIZE = [tuple(f['Images'].attrs['Image size / pixels'])]
+    IMAGE_SIZE = [f['Images'].attrs['Image size / pixels']]
     print(IMAGE_SIZE)
 
 # ******* EPI THRESHOLDING *******
