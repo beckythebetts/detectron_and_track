22db8a62a5b5a21173e375c24d65be26d814ccd4
beckythebetts <rebecca@bettscrossland.me.uk>
2024-09-25 13:39:19 +0100

    ~

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def show_segmentation(image_array, mask_array):
     image_array, mask_array = torch.tensor(np.stack((image_array, image_array, image_array), axis=-1)), torch.tensor(mask_array)
-    outline = mask_funcs.mask_outline(torch.where(mask_array>0, 1, 0, thickness=1))
+    outline = mask_funcs.mask_outline(torch.where(mask_array>0, 1, 0), thickness=1)
     image_array[outline][0] = torch.max(image_array)
     #print(image_array.shape, image_array.type)
     return image_array
