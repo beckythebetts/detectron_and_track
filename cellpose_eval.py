e6783b0e38f66c5b09bf1d701553d85e7317fb95
beckythebetts <rebecca@bettscrossland.me.uk>
2024-10-02 13:25:52 +0100

    ~

--- cellpose_eval.py
+++ cellpose_eval.py
@@ -10,21 +10,20 @@ import view_frame
 import utils
 
 
-def cellpose_eval(directory, model_name='model'):
+def cellpose_eval(directory, model=SETTINGS.CELLPOSE_MODEL / 'models' / 'model', thresholds=np.arange(0.5, 1.0, 0.05)):
     use_GPU = core.use_gpu()
     io.logger_setup()
-    model = models.CellposeModel(gpu=use_GPU, pretrained_model=str(SETTINGS.CELLPOSE_MODEL / 'models' / model_name))
+    model = models.CellposeModel(gpu=use_GPU, pretrained_model=str(model))
     im_names = [im.stem[:2] for im in directory.iterdir() if 'im' in im.stem]
     validation_ims = [io.imread(im) for im in directory.iterdir() if 'im' in im.name]
     channels = [0, 0]
     preds, flow, styles = model.eval(validation_ims, flow_threshold=0.2)
     for pred, im_name in zip(preds, im_names):
-        Image.fromarray(pred.astype(np.uint16)).save(str(directory/f'{model_name, im_name}pred.png'))
+        Image.fromarray(pred.astype(np.uint16)).save(str(directory/f'{im_name}pred.png'))
 
         #plt.imsave(str(directory/f'{im_name}pred.png'), pred, cmap='gray')
     true_masks = [io.imread(im) for im in directory.iterdir() if 'mask' in im.name]
     # NOTE, this is not AP as defined elsewhere
-    thresholds = np.arange(0.5, 1.0, 0.05)
     APs, TPs, FPs, FNs = metrics.average_precision(true_masks, preds, threshold=thresholds)
     precisions = TPs / (TPs+FPs)
     recalls = TPs / (TPs+FNs)
@@ -34,9 +33,9 @@ def cellpose_eval(directory, model_name='model'):
                          'Recall': recalls[i],
                          'F1': F1s[i]},
                         index=thresholds)
-        df.to_csv(str(directory / f'{model_name, im_name}_results.txt'), sep='\t')
+        df.to_csv(str(directory / f'{im_name}_results.txt'), sep='\t')
         #view_frame.show_frame(str(directory / f'{im_name}im.png'), str(directory /f'{im_name}pred.png'), str(directory /f'{im_name}_view.png'))
-        plt.imsave(str(directory /f'{model_name, im_name}_view.png'), utils.show_segmentation(np.array(validation_ims[i]), np.array(preds[i]).astype(np.int16), np.array(true_masks[i]).astype(np.int16)))
+        plt.imsave(str(directory /f'{im_name}_view.png'), utils.show_segmentation(np.array(validation_ims[i]), np.array(preds[i]).astype(np.int16), np.array(true_masks[i]).astype(np.int16)))
 
 def cellpose_eval_from_ims(directory):
     image = [io.imread(str(directory/'Images'/'snap01.png'))]
--- cellpose_kfold.py
+++ cellpose_kfold.py
@@ -2,28 +2,101 @@ from pathlib import Path
 import numpy as np
 import os
 import shutil
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+import cellpose_train
+import cellpose_eval
 
 class CellposeKfold:
     def __init__(self, directory):
         self.directory = directory
-        self.names = np.unique([name.stem[2:] for im in (directory / 'all').iterdir() if 'im' in im])
-        self.pairs = [[directory / 'all' / ('im', name, '.png'), directory / 'all' / ('mask', name, '.png')] for name in self.names]
+        self.names = np.unique([im.stem[:2] for im in (directory / 'all').iterdir() if 'im' in im.stem])
+        self.pairs = [[directory / 'all' / (name+'im'+'.png'), directory / 'all' / (name+'mask'+'.png')] for name in self.names]
 
     def split_datasets(self):
-        for im in self.names:
-            train_dir = directory / f'test_with_{im_name}' / 'train'
-            val_dir = directory / f'test_with_{im_name}' / 'validate'
+        for test_pair in self.pairs:
+            train_dir = self.directory / f'test_with_{test_pair[0].stem[:2]}' / 'train'
+            val_dir = self.directory / f'test_with_{test_pair[0].stem[:2]}' / 'validate'
             train_dir.mkdir(parents=True)
             val_dir.mkdir()
+            for item in test_pair:
+                shutil.copy(item, val_dir)
+            for train_pair in self.pairs:
+                if train_pair != test_pair:
+                    for item in train_pair:
+                        shutil.copy(item, train_dir)
 
-def move_im()
-def split_datasets(directory):
-    im_names = np.unique([name.stem[2:] for im in (directory / 'all').iterdir() if 'im' in im])
-    for im_name in im_names:
-        train_dir = directory / f'test_with_{im_name}' / 'train'
-        val_dir = directory / f'test_with_{im_name}' / 'validate'
-        train_dir.mkdir(parents=True)
-        val_dir.mkdir()
-        shutil.copy(directory / 'all' / ('im', im_name, '.png'), val_dir)
-        shutil.copy(directory / 'all' / ('ma', im_name, '.png'), val_dir)
+    def train_eval_datasets(self):
+        for dataset_dir in self.directory.iterdir():
+            if dataset_dir.name != 'all':
+                cellpose_train.cellpose_train(datset_dir)
+                cellpose_eval.cellpose_eval(datset_dir, model=dataset_dir / 'models' / 'model')
 
+    def get_results(self):
+        results = []
+        for dataset_dir in self.directory.iterdir():
+            if dataset_dir.name != 'all':
+                for file in dataset_dir.dir.iterdir():
+                    if file.suffix == '.txt':
+                        resuts.append(pd.read_csv(file, sep='\t', index_col=0))
+        results = pd.concat(results, axis=0)
+        self.means, self.stds = results.groupby(level=0).mean(), results.groupby(level=0).std()
+        self.means.to_csv(str(self.directory / 'results_means.txt'), sep='\t')
+        self.stds.to_csv(str(self.directory / 'results_stds.txt'), sep='\t')
+
+    def plot_results(self):
+        metrics = self.means.columns.values
+        thresholds = self.means.index.values
+        print(thresholds)
+        plt.rcParams["font.family"] = 'serif'
+        fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+
+        for ax, metric in zip(axs, metrics):
+            ax.plot(thresholds, self.means[metric], color='red', label='Cellpose')
+            ax.fill_between(thresholds, self.means[metric] - self.stds[metric],
+                            self.means[metric] + self.stds[metric], color='red', alpha=0.5, edgecolor='none')
+            ax.set_xlabel('IOU Threshold')
+            ax.set_ylabel(metric)
+            ax.grid(True)
+        plt.savefig(str(self.directory / 'results.png'))
+
+    def plot_losses(self):
+        train_losses = []
+        test_losses = []
+        file_names = [dataset_dir / 'losses.txt' for file in self.directory.iterdir() if file.name != 'all']
+        for dataset_dir in self.directory.iterdir():
+            if dataset_dir.name != 'all':
+                with open(datset_dir / 'loses.txt') as f:
+                    losses = json.load(f)
+                    train_losses.append(f['Train Losses'])
+                    test_losses.append(f['Validation Losses'])
+        train_losses = np.vstack(train_losses)
+        test_losses = np.vstack(test_losses)
+        train_means = np.mean(train_losses, axis=1)
+        test_means = np.mean(test_losses, axis=1)
+        train_stds = np.std(train_losses, axis=1)
+        test_stds = np.std(test_losses, axis=1)
+        iterations = np.arange(0, len(train_means))
+        test_iterations, test_means, test_stds = [[it, mean, std] for it, mean, std in zip(iterations, test_means, test_stds) if mean !=0]
+        plt.rcParams["font.family"] = 'serif'
+
+        plt.plot(test_iterations, test_means, color='red', label='Validation loss')
+        plf.fill_between(test_iterations, test_means-test_stds, test_means+test_stds, color='red', alpha=0.5, edgecolor='none')
+
+        plt.plot(iterations, train_means, color='red', label='Train loss')
+        plf.fill_between(iterations, train_means - train_stds, train_means + train_stds, color='navy', alpha=0.5,
+                         edgecolor='none')
+
+        plt.grid()
+        plt.legend()
+        plt.savefig(str(self.directory / 'av_loss_plot.png'))
+
+
+def main():
+    kfold = CellposeKfold(Path('cellpose_Models') / 'filter_kfold')
+    kfold.split_datasets()
+
+if __name__ == '__main__':
+    main()
