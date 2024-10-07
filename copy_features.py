e29f7d0ae128a47a83b5c0673b6e28891d25759f
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-10-07 09:53:51 +0100

    ~

--- copy_features.py
+++ copy_features.py
@@ -7,9 +7,13 @@ def copy_features(orig_dataset, new_dataset):
         data = orig['Features']
         with h5py.File(str(new_dataset), 'w') as new:
             orig.copy(data, new, 'Features')
+def del_stuff(dataset):
+    with h5py.File(str(dataset), 'r+') as f:
+        del f['Segmentations']
+        del f['Features']
 
 def main():
-    copy_features(Path('Datasets') / 'filter_test' / 'no_filter00.h5', Path('Datasets') / 'filter_test' / 'no_filter00feat.h5')
-
+    #copy_features(Path('Datasets') / 'filter_test' / 'no_filter00.h5', Path('Datasets') / 'filter_test' / 'no_filter00feat.h5')
+    del_stuff('Datasets/filter_test/no_filter01.h5')
 if __name__ == '__main__':
     main()
