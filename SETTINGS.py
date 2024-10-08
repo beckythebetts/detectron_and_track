21ac2c9c78166a2eadd8d18fcb4ce5a641b0f0d4
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-10-08 10:35:07 +0100

    ~

--- SETTINGS.py
+++ SETTINGS.py
@@ -10,9 +10,9 @@ MASK_RCNN_MODEL = Path("Models") / 'filter02'
 CELLPOSE_MODEL = Path("cellpose_Models") / '5ims'
 CLASSES = {'phase': 'Amoeba', 'epi': 'Yeast'}
 REMOVE_EDGE_CELLS = True
-with h5py.File(DATASET, 'r') as f:
-    NUM_FRAMES = f['Images'].attrs['Number of frames']
-    IMAGE_SIZE = f['Images'].attrs['Image size / pixels']
+# with h5py.File(DATASET, 'r') as f:
+#     NUM_FRAMES = f['Images'].attrs['Number of frames']
+#     IMAGE_SIZE = f['Images'].attrs['Image size / pixels']
 #     print(IMAGE_SIZE[0])
 # IMAGE_SIZE = [2048, 2048]
 
