5d76a08c78ec56476e6a9d8123801a33f4e67d35
Becky Betts <rjbetts1@sheffield.ac.uk>
2024-10-09 16:34:31 +0100

    ~

--- show_hdf5.py
+++ show_hdf5.py
@@ -97,8 +97,10 @@ def show_tracked_images_fast():
             f'\rFrame {i + 1}')
         sys.stdout.flush()
         print('\nseg size', sys.getsizeof(segmentation)/(1024**3))
-        expanded_segmentation = [segmentation[segmentation==idx] for idx in np.unique(segmentation)]
+        #expanded_segmentation = [segmentation[segmentation==idx] for idx in np.unique(segmentation)]
+        expanded_segmentation = (segmentation == np.unique(segmentation))
         #expanded_segmentation = (segmentation.unsqueeze(0) == torch.unique(segmentation).view(-1, 1, 1))
+        print(expanded_segmentation.shape)
         print('expanded size', sys.getsizeof(expanded_segmentation)/(1024**3))
         #outlines = mask_funcs.mask_outline(expanded_segmentation, thickness=1)
             # print(expanded_segmentation.dtype)
