d51e280e8f0f24d57128fb8387aa31acfa635765
beckythebetts <rebecca@bettscrossland.me.uk>
2024-10-10 13:55:22 +0100

    ~

--- show_hdf5.py
+++ show_hdf5.py
@@ -105,7 +105,7 @@ def show_tracked_images_fast():
         sys.stdout.flush()
         outlines = mask_funcs.mask_outlines(segmentation).int()
         outlines = LUT[outlines]
-        phase_image =  torch.where(outlines>0, outline, phase_image)
+        phase_image =  torch.where(outlines>0, outlines, phase_image)
         #phase_image[outlines>0] = outlines[outlines>0].int()
         tracked[i] = phase_image.cpu().numpy()
         # def find_mask_boundary(mask):
