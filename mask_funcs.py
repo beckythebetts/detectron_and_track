5a6650cf8d31af4edeafa532e0346323b1aa921a
beckythebetts <rebecca@bettscrossland.me.uk>
2024-10-11 11:44:39 +0100

    ~

--- mask_funcs.py
+++ mask_funcs.py
@@ -99,7 +99,7 @@ def cal_iou(mask1, mask2):
 #     return outline
 
 def mask_outline(mask, thickness=3):
-    expanded_mask = (F.max_pool2d(mask.float(), kernel_size=2*thickness+1, stride=1, padding=thickness) > 0)
+    expanded_mask = (F.max_pool2d(mask.float().unsqueeze(0), kernel_size=2*thickness+1, stride=1, padding=thickness) > 0)
     outline = (expanded_mask.byte().squeeze() - mask).bool()
     return outline
 
